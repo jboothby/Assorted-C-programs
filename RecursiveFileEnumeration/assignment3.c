58da31827cb6b401a76afb310b801c9085b46292
Joseph Boothby <josephboothby37@gmail.com>
2021-02-17 05:32:55 -0800

    Finished outline of readable method

--- RecursiveFileEnumeration/assignment3.c
+++ RecursiveFileEnumeration/assignment3.c
@@ -19,6 +19,10 @@ int main(int argc, char *argv[]){
 	// Check arguments and set start directory appropriately
 	char initialDirectory[256];
 	if( argc > 1 ){
+		if( strlen(argv[1]) > 255){
+				fprintf(stderr,"Supplied directory name too long\n");
+				return( -1 );
+		}
 		strncpy(initialDirectory, argv[1], strlen(argv[1]) + 1);
 		printf("Starting at directory %s\n", initialDirectory);
 	}else{
@@ -33,6 +37,39 @@ int main(int argc, char *argv[]){
 		fprintf(stderr, "%s\n", strerror(errno));
 		return( -errno);
 	}
+	closedir(dirp);
+
+	// Call recursive function 'readable'
+	printf("There are %d readable files in the file structure starting at %s\n",
+			readable(initialDirectory), initialDirectory);
+
+}
+
+int readable(char *inputPath){
+
+	DIR *dirp = opendir(inputPath);
+	int count = 0;
+
+	// skip empty directories or those that cannot be opened
+	if( dirp == NULL ){
+	      return count;
+	}
+
+	struct dirent* direntp;
+	//  loop until direntp is NULL (End-of-directory or error
+	errno = 0;
+	while( (direntp = readdir(dirp)) != NULL ){
+
+		// skip "." and ".." directories
+		if( strcmp( direntp->d_name, ".") == 0 || strcmp( direntp->d_name, "..") == 0){
+				printf("Skipping %s", direntp->d_name);
+				continue;
+		}
+
+
+	}
+	closedir(dirp);
+	return count;
 
 }
 
