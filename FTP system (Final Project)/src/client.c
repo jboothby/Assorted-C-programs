f466304c9075d7f940f316bde419af0cc6f7169b
= <=>
2021-04-11 22:37:02 -0700

    Initial connection to server complete. Able to write Q across fd

--- FTP system (Final Project)/src/client.c
+++ FTP system (Final Project)/src/client.c
@@ -1,5 +1,4 @@
 /* Client side of the FTP System */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -14,17 +13,61 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define PORTNUM 37896   // Port number for connection to server
+/* ------------------- Defines and Globals ---------------------- */
+#define PORTNUM 37897   // Port number for connection to server
 
 static int debug = 0;   // Debug flag for verbose output
 
-char* parseArgs(int c, char** v);
+/* --------------------- Function Prototypes -------------------- */
+char* parseArgs(int c, char** v);           // Parse command line arguments
+int attemptConnection( const char *address);          // Handle spinning up client connection
 
 int main(int argc, char* argv[]){
 
-    char* hostname;
-    hostname = parseArgs(argc, argv);
+    const char* hostname = parseArgs(argc, argv);
+    attemptConnection(hostname);
     
+    return 0;
+}
+
+/* Create the sockets, resolve address, then connect on specified port */
+int attemptConnection( const char* address){
+    int sockfd;                             // socket file descriptor
+    int err;                                // Holds error code
+
+    struct addrinfo serv, *actualData;      // address info for the server
+    memset(&serv, 0, sizeof(serv));         
+
+    serv.ai_socktype = SOCK_STREAM;         
+    serv.ai_family = AF_INET;   
+    
+    char port[6];                           // Length 6 because port max is 65535 + \n
+    sprintf(port, "%d", PORTNUM);           // convert portnum to string for dns lookup
+    
+    // Dns lookup
+    err = getaddrinfo(address, port, &serv, &actualData);
+    if( err ){
+        fprintf(stderr, "Error: %s\n", gai_strerror(err));
+        exit(-1);
+    }
+
+    // Create socket with server 
+    sockfd = socket(actualData -> ai_family, actualData -> ai_socktype, 0);
+    if( sockfd < 0 ){
+        perror("socket");
+        exit(-1);
+    }
+
+    // Attempt server connection
+    err = connect( sockfd, actualData -> ai_addr, actualData -> ai_addrlen);
+    if( err < 0 ){
+        perror("connect");
+        exit(-1);
+    }
+
+    write(sockfd, "Q\n", 3);
+    printf("Wrote Q\\n to sockfd\n");
+
     return 0;
 }
 
@@ -57,7 +100,6 @@ char* parseArgs(int argnum, char** arguments){
         return(arguments[1]);
     }
 
-    // If none of the above, then error out
     fprintf(stderr, "%s\n", usageMsg);
     exit(-1);
 }
